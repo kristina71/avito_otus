5c5b752098eb08fe95aefe273db2f7be39774a81
Кристина <kristina.ezikova@yandex.ru>
2022-05-16 00:13:05 +0300

    Add HW2 - Uppack string

--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -17,10 +17,17 @@ func TestUnpack(t *testing.T) {
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
 		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `qwe\4\5`, expected: `qwe45`},
+		{input: `qwe\45`, expected: `qwe44444`},
+		{input: `qwe\\5`, expected: `qwe\\\\\`},
+		{input: `qwe\\\3`, expected: `qwe\3`},
+		// added tests
+		{input: ``, expected: ``},
+		{input: "aaaa", expected: "aaaa"},
+		{input: "a4", expected: "aaaa"},
+		{input: "\\", expected: ""},
+		{input: `\\`, expected: "\\"},
+		{input: `\`, expected: ``},
 	}
 
 	for _, tc := range tests {

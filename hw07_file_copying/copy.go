cd16c6c5c62332df4feb9930259ac3deeaac86bc
Кристина <kristina.ezikova@yandex.ru>
2022-07-12 23:44:19 +0300

    HW-7 - file copying

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,124 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"github.com/cheggaaa/pb/v3"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
 )
 
 var (
-	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrInvalidPath           = errors.New("invalid path")
+	ErrSetOffset             = errors.New("unable to set offset")
+	ErrFileStat              = errors.New("unable to get file's stat")
+	ErrCopy                  = errors.New("copy failed")
 )
 
-func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+func min(a, b int64) int64 {
+	if a > b {
+		return b
+	}
+
+	return a
+}
+
+func closeFile(f io.Closer) {
+	if err := f.Close(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func openFile(filePath string) *os.File {
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return file
+}
+
+func readFile(file *os.File) []byte {
+	expData, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return expData
+}
+
+func removeFile(tmpFile *os.File) {
+	err := os.Remove(tmpFile.Name())
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func tempFile(pattern string) *os.File {
+	baseDir := "/tmp"
+	tmpFile, err := ioutil.TempFile(baseDir, pattern)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return tmpFile
+}
+
+func createDir(inputPath string) {
+	err := os.MkdirAll(inputPath, 0777)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Copy(fromPath string, toPath string, offset int64, limit int64) error {
+	in, err := os.Open(fromPath)
+	if err != nil {
+		return fmt.Errorf("%w: open for reading failed %s", ErrInvalidPath, err)
+	}
+	defer in.Close()
+
+	inFileInfo, err := in.Stat()
+	if err != nil {
+		return fmt.Errorf("%w: %s", ErrFileStat, err)
+	}
+	inFileSize := inFileInfo.Size()
+
+	if offset > 0 {
+		if offset > inFileSize {
+			return fmt.Errorf(
+				"%w: given offset %d to large, file %q has len %d",
+				ErrOffsetExceedsFileSize,
+				offset,
+				fromPath,
+				inFileSize,
+			)
+		}
+		if _, err := in.Seek(offset, io.SeekStart); err != nil {
+			return fmt.Errorf("%w: set position %d in file %q failed %s", ErrSetOffset, offset, from, err)
+		}
+	}
+
+	var reader io.Reader = in
+	needToCopyBytes := inFileSize
+
+	if limit > 0 {
+		reader = io.LimitReader(in, limit)
+		needToCopyBytes = min(limit, inFileSize-offset)
+	}
+
+	writer, err := os.Create(toPath)
+	if err != nil {
+		return fmt.Errorf("%w: unable to open file %q for writing %s", ErrInvalidPath, toPath, err)
+	}
+	defer closeFile(writer)
+
+	bar := pb.Full.Start64(needToCopyBytes)
+	defer bar.Finish()
+
+	barReader := bar.NewProxyReader(reader)
+	if _, err = io.Copy(writer, barReader); err != nil {
+		return fmt.Errorf("%w: %s", ErrCopy, err)
+	}
+
 	return nil
 }
--- hw07_file_copying/go.mod
+++ hw07_file_copying/go.mod
@@ -1,3 +1,11 @@
-module github.com/fixme_my_friend/hw07_file_copying
+module github.com/kristina71/avito_otus/hw07_file_copying
 
 go 1.16
+
+require (
+	github.com/cheggaaa/pb/v3 v3.0.8
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/stretchr/testify v1.8.0
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+)

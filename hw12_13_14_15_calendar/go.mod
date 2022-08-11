1bd0e6576ac7450ab86173f486f460d3c53f8ef2
Кристина <kristina.ezikova@yandex.ru>
2022-08-12 00:31:55 +0300

    Add dependecies

--- hw12_13_14_15_calendar/go.mod
+++ hw12_13_14_15_calendar/go.mod
@@ -1,3 +1,9 @@
 module github.com/fixme_my_friend/hw12_13_14_15_calendar
 
 go 1.16
+
+require (
+	github.com/Masterminds/squirrel v1.5.3 // indirect
+	github.com/jmoiron/sqlx v1.3.5 // indirect
+	go.uber.org/zap v1.22.0 // indirect
+)
--- hw12_13_14_15_calendar/internal/storage/sql/storage.go
+++ hw12_13_14_15_calendar/internal/storage/sql/storage.go
@@ -1,20 +1,39 @@
 package sqlstorage
 
-import "context"
+import (
+	"context"
+	"fmt"
+	"github.com/jmoiron/sqlx"
+	"go.uber.org/zap"
+)
 
-type Storage struct { // TODO
+type Storage struct {
+	dsn string
+	db  *sqlx.DB
 }
 
 func New() *Storage {
-	return &Storage{}
+	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		"cfg.Host", "cfg.Port", "cfg.User", "cfg.Password", "cfg.DBName",
+	)
+	return &Storage{
+		dsn: dsn,
+	}
 }
 
 func (s *Storage) Connect(ctx context.Context) error {
-	// TODO
+	db, err := sqlx.ConnectContext(ctx, "pgx", s.dsn)
+	if err != nil {
+		return err
+	}
+
+	s.db = db
+	zap.L().Info("connect to db")
 	return nil
 }
 
 func (s *Storage) Close(ctx context.Context) error {
-	// TODO
-	return nil
+	zap.L().Info("close sql connection to db")
+
+	return s.db.Close()
 }
